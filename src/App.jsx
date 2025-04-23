31dade7cec04d4251fb6be649f74bc92870ff574
Hazhar Maroufi <hazhmar@gmail.com>
2025-04-23 17:42:37 +0330

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navigate, Route, Routes } from "react-router"; // وقتی روتز رو پایین مینوسیم خودش اینو ایمپورت میکنه
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import PageNotFound from "./pages/404";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      {/* صفحات رو که داخل پیجز ساختیم یکی یکی اضافه میکنیم پایین */}
+      <Route index element={<Navigate to="/products" replace />} />
+      {/* برای اینکه هرکی اومد صفحه اصلی بفرسته تو پروداکتز و لوپ نخوره از نویگیت و ریپلیس استفاده کردیم */}
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<DetailsPage />} />
+      {/* برای اینکه لینک دینامیک بشه بعد پت دو نقطه قرای میدیم و هرچیز خواستیم بعدش مینویسیم  */}
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router"; // در همون ابتدا برای مسیر ها نصبش میکنیم
+import App from "./App.jsx";
+import "./global.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* بالا که نصبش کردم اینجا دور اپ قرادش میدم که بتونم داخل اپ ازش استفاده کنم  */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
