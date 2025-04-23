d9c3970faa1766e949afa8ed191996d562805d53
Hazhar Maroufi <hazhmar@gmail.com>
2025-04-23 17:49:53 +0330

    config axios

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,13 @@
 import axios from "axios";
 
-const api = axios.create({baseURL : "https://fakestoreapi.com/products"})
+const api = axios.create({baseURL : "https://fakestoreapi.com"})
+// برای اینکه بتونیم تمیز تر به ای پی ای دسترسی داشته باشیم
+
+api.interceptors.response.use(
+    (response) => response.data   
+, error => Promise.reject(error))
+
+//  در بالا برای اطلاعاتی که دریافت میکنیم فقط اینترسپتر نوشتیم که در مسیر دریافت ای پی ای قرار میگیره
+//  و باعث میشه از ریسپانس فقط بخش دیتا رو برای ما دریافت کنه
+
+export default api;
